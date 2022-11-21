ad1392a6bcd7fdcfb337194b60b33ca6b33224df
Jawaid Hakim <himalayahall@gmail.com>
2022-11-21 00:44:26 -0500

    add y-var selection
    Signed-off-by: Jawaid Hakim <himalayahall@gmail.com>

--- src/OWID_ShinyApp1.R
+++ src/OWID_ShinyApp1.R
@@ -10,62 +10,67 @@ source("OurWorldInData_Common.R", echo = FALSE)
 covid <- LoadDataset(verbose = FALSE)
 glimpse(covid)
 
-ui <- fluidPage(
-  titlePanel("COVID"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      helpText("COVID Deaths."),
-
-    checkboxGroupInput(
-                        "locations",
-                        label = h3("Locations group"),
-                        choices = unique(covid$location),
-                        selected = c('United States', 'Great Britain', 'France', 'Japan', 'Germany', 'India')
+ui <- fluidPage(titlePanel("COVID"),
+                
+                sidebarLayout(
+                    sidebarPanel(
+                        helpText("COVID Deaths."),
+                        
+                        radioButtons(
+                            "metric",
+                            label = h3("Mertic"),
+                            choices = c(
+                                'total_deaths_per_million',
+                                'excess_mortality_cumulative',
+                                'population',
+                                'hosp_patients_per_million',
+                                'total_cases_per_million',
+                                'positive_rate',
+                                'total_vaccinations_per_hundred'
+                            ),
+                            selected = c('total_deaths_per_million')
+                        ),
+                        
+                        checkboxGroupInput(
+                            "locations",
+                            label = h3("Locations group"),
+                            choices = unique(covid$location),
+                            selected = c(
+                                'United States',
+                                'Great Britain',
+                                'France',
+                                'Japan',
+                                'Germany',
+                                'India'
+                            )
+                        )
                     ),
+                    
+                    mainPanel(plotlyOutput("locationPlot"))
+                ))
 
-      # selectInput("var", 
-      #             label = "Choose a variable to display",
-      #             choices = c("Percent White", 
-      #                         "Percent Black",
-      #                         "Percent Hispanic", 
-      #                         "Percent Asian"),
-      #             selected = "Percent White"),
-      
-      sliderInput("range", 
-                  label = "Range of interest:",
-                  min = 0, max = 100, value = c(0, 100))
-    ),
-    
-    mainPanel(
-      plotlyOutput("locationPlot")
-    )
-  )
-)
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
-    
     output$locationPlot <- renderPlotly({
-            covid |> filter(location %in% input$locations) |>
+        covid |>
+            filter(location %in% input$locations) |>
             arrange(date) |>
             ggplot(aes(
                 x = date,
-                y = total_deaths_per_million,
+                y = .data[[input$metric]],
                 color = location
             )) +
             geom_line(size = 0.5) +
             scale_x_date(
                 date_breaks = "1 year",
-                date_minor_breaks = '1 month', 
+                date_minor_breaks = '1 month',
                 date_labels = "%Y"
             ) +
-            scale_y_continuous(limits = c(0, 5000),
-                               labels = scales::label_comma()) +
             labs(
-                title = "COVID Deaths Per Million",
+                title = "COVID",
                 subtitle = "2020 - 2023",
                 caption = "source: Our World In Data",
-                y = "Deaths"
+                y = "Metric"
             ) +
             theme_minimal()
     })
